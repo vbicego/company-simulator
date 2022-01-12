2b7b58012014d664b8a7f46f24103c6680f722c6
Victor Bicego <vbicego@evoila.de>
2022-01-12 09:53:01 +0100

    First commit

---
+++ src/main/java/de/evoila/companySimulator/CompanySimulatorApplication.java
@@ -0,0 +1,13 @@
+package de.evoila.companySimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanySimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanySimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/evoila/companySimulator/CompanySimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.evoila.companySimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
